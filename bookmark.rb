8ba831d70fe8489d5a6ff9ba900d94ba86c62b78
ajbuchanjr <ajbuchanjr@gmail.com>
2015-04-26 21:39:25 -0400

    finished, added test

--- bookmark.rb
+++ bookmark.rb
@@ -1,16 +1,27 @@
 #An initializer that accepts url and title parameters:
   #it should set instance variables named url, title
   #it should set a read only variable named last_visited
-  #A method named visit! which sets last_visited to the current time and date
+#A method named visit! which sets last_visited to the current time and date
     #(hint: the Time.now method returns an instance of Time)
 
-class Bookmark < ApplicationController
+class Bookmark
+  attr_accessor :url, :title
+  attr_reader :last_visited
 
-  def set_url
-    @url = Book.find(params[:id])
-  end
-  def set_title
-    @title = Book.find(params[:id])
-  end
+def initialize(url, title)
+  @url  = url
+  @title = title
+  @last_visited = visit!
+end
+
+def visit!
+  last_visited = Time.now.strftime("%m/%d/%Y %H:%M:%S")
+end
+
+#Testing...Test
+#bookmark = Bookmark.new('http://www.yahoo.com', 'Yahoo')
+#puts bookmark.url
+#puts bookmark.title
+#puts bookmark.last_visited
 
 end
